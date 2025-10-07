28dfce086b989a915f66df911a2867ca93a9d0e7
pchp250g8-home <pchp250g8@tutamail.com>
2025-10-07 11:31:34 +0300

    HelloWorld2 Web Application

---
+++ HelloWorld2.php
@@ -0,0 +1,6 @@
+<?php
+    echo("Hello, World!!!<br>");
+    echo("PHP Test Web Page!!!<br>");
+    echo("Used Query Method:".$_SERVER["REQUEST_METHOD"]."<br>");
+    echo("Hi, ".$_GET["TextEdit1"]."!!!<br>");
+?>
