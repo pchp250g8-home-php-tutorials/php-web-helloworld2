d035020d62e51db667697e9600e1d81b4a2ed4ee
pchp250g8-home <pchp250g8@tutamail.com>
2025-10-07 12:28:30 +0300

    Added POST Method Form. Added tag "reference" to PHP Code

--- HelloWorld2.php
+++ HelloWorld2.php
@@ -2,5 +2,6 @@
     echo("Hello, World!!!<br>");
     echo("PHP Test Web Page!!!<br>");
     echo("Used Query Method:".$_SERVER["REQUEST_METHOD"]."<br>");
-    echo("Hi, ".$_GET["TextEdit1"]."!!!<br>");
+    echo("Hi, ".$_REQUEST["TextEdit1"]."!!!<br>");
+    echo("<a href=\"index.html\">Enter Data Again</a>");
 ?>
